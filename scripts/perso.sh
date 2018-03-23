1b7d4213779c79fbb4294bfcea66814eb80f89ab
Michel Daviot <michel@daviot.info>
2018-03-23 19:12:20 +0100

    :bug: properly run perso as vagrant, not root

--- scripts/perso.sh
+++ scripts/perso.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-su - vagrant
-
-cd
-
+su vagrant <<EOF
+cd 
 git clone https://github.com/tyrcho/dotfiles.git
-
 cp -r dotfiles/. .
 
 git config --global user.email "michel@daviot.info"
 git config --global user.name "Michel Daviot"
+
+EOF
