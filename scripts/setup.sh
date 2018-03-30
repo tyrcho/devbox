55a67554810ec55ce069b9d9bbd9e624724c38aa
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-03-30 17:38:07 +0200

    :sparkles: autologin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,13 @@ cat <<EOT >> /etc/sudoers
 Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"
 %wheel ALL=(ALL) NOPASSWD: ALL
 EOT
+
+#autologin
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+
+cat <<EOT > /etc/systemd/system/getty@tty1.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin vagrant --noclear %I $TERM
+EOT
