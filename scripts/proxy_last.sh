29276490eb5fcab38917750ab2aa706066ed5983
Michel Daviot <michel@daviot.info>
2018-04-03 16:09:03 +0200

    :sparkles: proxy for Chrome

--- scripts/proxy_last.sh
+++ scripts/proxy_last.sh
@@ -5,3 +5,5 @@ cat <<EOT >> /home/vagrant/.ssh/config
 Host github.com gitlab.com
         ProxyCommand corkscrew proxy-internet.localnet 3128 %h %p
 EOT
+
+echo "--proxy-pac-url=http://proxyconf.localnet/awl.pac" >> ~/.config/chrome-flags.conf
