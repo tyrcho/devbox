60ab847f5145e677804041f6c4429d108c059765
Michel Daviot <michel@daviot.info>
2018-04-03 22:37:54 +0200

    :fire: dead comments

--- scripts/install_long.sh
+++ scripts/install_long.sh
@@ -24,11 +24,3 @@ install docker
 sudo systemctl enable docker
 #sudo systemctl start docker
 sudo usermod -aG docker vagrant
-
-#TODO: Proxy configuration
-#Create a Drop-in snippet with the following content:
-#
-#/etc/systemd/system/docker.service.d/proxy.conf
-#[Service]
-#Environment="HTTP_PROXY=192.168.1.1:8080"
-#Environment="HTTPS_PROXY=192.168.1.1:8080"
